63dd85097064eae30beab4170bfab0b24a4fd665
sherlock <vlagria3@gmail.com>
2022-03-21 20:44:03 +0800

    gwapo soyosty

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PlaceController extends Controller
 {
@@ -12,7 +13,37 @@ class PlaceController extends Controller
 
     public function storePlace(Request $request){
         $store = $_POST['information']; 
-        dd($store); die();
-        return dd($store);
+        $desc = $_POST['desc'];
+        $arr = array();
+        $d = "";
+        $s = "";
+
+        $store_size = sizeof($_POST['information']); 
+        $desc_size = sizeof($_POST['desc']);
+        
+        for ($i=0; $i < $store_size; $i++) { 
+            for ($j=0; $j < $desc_size ; $j++) { 
+                    if ($i === $j) {
+                        $arr += array($desc[$j] => $store[$i]);
+                        $d = $desc[$j];
+                        $s = $store[$i];
+
+                        DB::table('second_tables')->insert([
+                            'reference_table' => $d,
+                            'value' => $s
+                        ]);
+                        $json = json_encode($arr);
+
+
+                    }
+            }
+        }
+        
+        DB::table('first_tables')->insert([
+            'json_detail' => $json
+        ]);
+        
+        dd($json); die();
+        return dd($json);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,14 +19,17 @@
             <div id="fields-container">
                 <div id="clone">
                     <div id="input-fields">
-                        <label for="exampleFormControlInput1">Name</label>
-                        <input type="text" class="form-control p-info" id="information" name="information[]"  placeholder="Add Name">
+                        <label for="exampleFormControlInput1" id="label">Name</label>
+                        <input type="hidden" class="desc" id="desc" name="desc[]" value="Name">
+                        <input type="text" class="form-control p-info" id="information" name="information[]"  placeholder="Add Information">
                     </div>
                 </div>
             </div>
         </div>
         <br>
     </div>
+        
+    <div class="container">
         <div class="form-group">
             <button type="submit" id="submit" class="btn btn-success">Insert Data</button>
         </div>
@@ -34,9 +37,50 @@
 
       <br>
       <div class="form-group">
-        <button type="button" id="warning" class="btn btn-warning btn-add__field">Add Field</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Add Field
+          </button>
     </div>
-
+          
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <form class="" action="" method="POST">
+                            @csrf
+                            <div class="form-group fields-input">
+                                <div id="fields-container">
+                                    <div id="clone">
+                                        <div id="input-fields">
+                                            <label for="exampleFormControlInput1" >Add Description Reference</label>
+                                            <input type="text" class="form-control p-info" id="description" placeholder="Add Reference Name">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary btn-add__field">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script>
@@ -47,7 +91,8 @@
 
         $('.btn-add__field').on('click', function(){
             alert('add field');
-            jQuery('#fields-container').last().clone(true).find("input:text").val("").end().appendTo('#input-fields').val("");
+            var desc = $('#description').val();
+            jQuery('#input-fields').last().clone(true).find('#information').val("").end().find("label").text(desc).end().find('#desc').val(desc).end().appendTo('#fields-container').val("");
         });
     </script>
 </body>
